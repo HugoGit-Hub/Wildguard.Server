aa01a7bb8e284cff26bb916c0dba6ccb81039c6e
hugo <hugo.decuq.34@icloud.com>
2024-10-24 15:04:25 +0200

    Create user entity persistence

---
+++ src/Infrastructure/Users/UserEntity.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infrastructure.Users;
+
+[Table("Users")]
+public class UserEntity(
+    int id,
+    string firstName,
+    string lastName,
+    string email,
+    string password,
+    int role)
+{
+    [Key]
+    public int Id { get; set; } = id;
+
+    [Required]
+    [MaxLength(64)]
+    public string FirstName { get; set; } = firstName;
+
+    [Required]
+    [MaxLength(64)]
+    public string LastName { get; set; } = lastName;
+
+    [Required]
+    [MaxLength(64)]
+    public string Email { get; set; } = email;
+
+    [Required]
+    [MaxLength(256)]
+    public string Password { get; set; } = password;
+
+    [Required]
+    public int Role { get; set; } = role;
+}
